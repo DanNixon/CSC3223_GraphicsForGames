db0f636c9b556eadee3ef04bd1a9670c2e35a50e
Dan Nixon <dan@dan-nixon.com>
2015-12-15 13:55:09 +0000

    aTry to fix

--- OpenGLRasteriser/OpenGLGraphics/split_geometry.glsl
+++ OpenGLRasteriser/OpenGLGraphics/split_geometry.glsl
@@ -30,14 +30,23 @@ vec4 triPos(float s, float t)
   return s * a + t * b + (1 - s - t) * c;
 }
 
-void cubeVertex(mat4 mvp, vec4 pos, vec3 vPos)
+vec2 triTexCoord(float s, float t)
 {
-  OUT.texCoord = (vPos.xy / 2) + 0.5;
+  vec2 a = IN[0].texCoord;
+  vec2 b = IN[1].texCoord;
+  vec2 c = IN[2].texCoord;
+
+  return s * a + t * b + (1 - s - t) * c;
+}
+
+void cubeVertex(mat4 mvp, vec4 pos, vec2 tex, vec3 vPos)
+{
+  OUT.texCoord = tex + vPos.xy;
   gl_Position = mvp * vec4(pos.xyz + vPos, 1);
   EmitVertex();
 }
 
-void cubeAtPoint(vec4 p, float s)
+void cubeAtPoint(vec4 p, float s, vec2 tex)
 {
   vec3 upper = vec3(0, 0, s);
   vec3 lower = vec3(0, 0, -s);
@@ -47,27 +56,27 @@ void cubeAtPoint(vec4 p, float s)
   vec3 back = vec3(0, -s, 0);
 
   mat4 mvp = projMatrix * viewMatrix * modelMatrix;
-
+  
   // First strip (front and side faces)
-  cubeVertex(mvp, p, upper + left + back);
-  cubeVertex(mvp, p, lower + left + back);
-  cubeVertex(mvp, p, upper + left + front);
-  cubeVertex(mvp, p, lower + left + front);
-  cubeVertex(mvp, p, upper + right + front);
-  cubeVertex(mvp, p, lower + right + front);
-  cubeVertex(mvp, p, upper + right + back);
-  cubeVertex(mvp, p, lower + right + back);
+  cubeVertex(mvp, p, tex, upper + left + back);
+  cubeVertex(mvp, p, tex, lower + left + back);
+  cubeVertex(mvp, p, tex, upper + left + front);
+  cubeVertex(mvp, p, tex, lower + left + front);
+  cubeVertex(mvp, p, tex, upper + right + front);
+  cubeVertex(mvp, p, tex, lower + right + front);
+  cubeVertex(mvp, p, tex, upper + right + back);
+  cubeVertex(mvp, p, tex, lower + right + back);
   EndPrimitive();
 
   // Second strip (top, bottom and side faces)
-  cubeVertex(mvp, p, upper + left + front);
-  cubeVertex(mvp, p, upper + right + front);
-  cubeVertex(mvp, p, upper + left + back);
-  cubeVertex(mvp, p, upper + right + back);
-  cubeVertex(mvp, p, lower + left + back);
-  cubeVertex(mvp, p, lower + right + back);
-  cubeVertex(mvp, p, lower + left + front);
-  cubeVertex(mvp, p, lower + right + front);
+  cubeVertex(mvp, p, tex, upper + left + front);
+  cubeVertex(mvp, p, tex, upper + right + front);
+  cubeVertex(mvp, p, tex, upper + left + back);
+  cubeVertex(mvp, p, tex, upper + right + back);
+  cubeVertex(mvp, p, tex, lower + left + back);
+  cubeVertex(mvp, p, tex, lower + right + back);
+  cubeVertex(mvp, p, tex, lower + left + front);
+  cubeVertex(mvp, p, tex, lower + right + front);
   EndPrimitive();
 }
 
@@ -83,7 +92,7 @@ void main()
       if (s + t > 1)
         continue;
 
-      cubeAtPoint(triPos(s, t) * ap, d);
+      cubeAtPoint(triPos(s, t) * ap, d, triTexCoord(s, t));
     }
   }
 }
